3f6d4aff15b779f80ac74890896766679fe1b681
Jonatank28 <jonatankaleidakk28@gmail.com>
2023-07-26 21:01:19 -0300

    feat: first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            backgroundColor: {
+                primary: "var(--bg-primary)",
+            },
+            fontFamily: {
+                primary: ["Montserrat", "sans-serif"],
+                secondary: ["Poppins", "sans-serif"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
