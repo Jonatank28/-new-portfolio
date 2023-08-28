b75dee51372d55b75764f62f0f266cf0555aedf7
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-08-28 19:05:35 -0300

    fix: erro build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,6 @@
-'use client'
 import './globals.css'
 
 import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-
-const poppins = Poppins({
-    subsets: ['latin'],
-    weight: ['200', '300', '400', '700'],
-    variable: '--font-poppins',
-})
 
 export const metadata: Metadata = {
     title: 'Portifólio',
@@ -22,7 +14,7 @@ export default function RootLayout({
     return (
         <html lang="pt-br">
             <body
-                className={`${poppins.variable} font-poppins bg-primary text-primary duration-300 ease-out delay-100 transition-all`}
+                className={`  bg-primary text-primary duration-300 ease-out delay-100 transition-all`}
             >
                 {children}
             </body>
