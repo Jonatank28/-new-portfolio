efe6087e602a9f000813053a4b52b393cbed85f3
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-08-28 18:58:17 -0300

    fix erro build[

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-// 'use client'
+'use client'
 import './globals.css'
 import { ThemeProvider } from 'next-themes'
 import { LanguageProvider } from '@/context/language'
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,13 @@
 'use client'
-
-import Footer from '@/components/Footer'
 import Header from '@/components/Header'
-import Projects from '@/components/Projects'
 import FixedContacts from '@/components/FixedContacts'
-import About from '@/components/About'
 import ScrollToTop from '@/components/utilities/ScrollToTop'
-import Contact from '@/components/Contact/page'
 
 export default function Home() {
     return (
         <main>
             <Header />
-            <div className="flex flex-col md:gap-[150px]">
-                {/* <Presentation /> */}
-                <About />
-                <Projects />
-                <Contact />
-                <Footer />
-            </div>
+
             {/* // Menu fixo que fica ao lado esquerdo inferior */}
             <ScrollToTop />
             <FixedContacts />
